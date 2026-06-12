8620bd71b53e9fb5030d53951d8a48ffdb024a74
Guilhermeaap <guilhermeantonioap@gmail.com>
2026-06-12 15:21:13 -0300

    feat: update sidebar header to display user initials and improve layout structure

--- resources/js/components/AppSidebarHeader.vue
+++ resources/js/components/AppSidebarHeader.vue
@@ -1,34 +1,45 @@
 <script setup>
 // Imports
-import { logout } from '@/routes'
-import { Link } from '@inertiajs/vue3'
-import { defineEmits, onMounted, ref } from 'vue'
-import { usePage } from '@inertiajs/vue3'
+import { logout } from '@/routes';
+import { Link } from '@inertiajs/vue3';
+import { defineEmits, onMounted, ref } from 'vue';
+import { usePage } from '@inertiajs/vue3';
 // Emits
 const emit = defineEmits(['toggle-sidebar']);
 
+// Variáveis
+const page = usePage();
+const user = page.props.auth.user;
+const siglaNome = ref('');
+const userAbreviado = ref('');
+const openMenuProfile = ref(false);
 
-// Variáveis    
-const page = usePage()
-const user = page.props.auth.user
-const siglaNome =ref('')
-
-onMounted (() => {
-    getSiglaNome(page.props.auth.user.name)
-})
+onMounted(() => {
+    getSiglaNome(page.props.auth.user.name);
+    getPrimeiroUltimoNome();
+});
 
 function getSiglaNome(nome) {
-    const parts = nome.trim().split(' ')
-    const firstLetter = parts[0]?.[0] || ''
-    const secondLetter= parts[1]?.[0] || ''
+    const parts = nome.trim().split(' ');
+    const firstLetter = parts[0]?.[0] || '';
+    const secondLetter = parts[1]?.[0] || '';
 
-    return siglaNome.value = firstLetter + secondLetter
+    return (siglaNome.value = firstLetter + secondLetter);
+}
 
+function getPrimeiroUltimoNome() {
+    const parts = page.props.auth.user.name.trim().split(' ');
+    const firstLetter = parts[0] || '';
+    const lastLetter = parts.at(-1);
+
+    return (userAbreviado.value = firstLetter + ' ' + lastLetter);
 }
 </script>
 
 <template>
-    <div class="flex h-[5vh] w-full items-center bg-primary p-2 drop-shadow-md shadow-md ">
+    <div
+        class="flex h-[5vh] w-full items-center bg-primary p-2 shadow-md drop-shadow-md"
+    >
         <div class="flex w-full items-center">
             <i
                 class="pi pi-bars cursor-pointer text-secondary hover:text-secondary-hover"
@@ -44,16 +55,31 @@ function getSiglaNome(nome) {
                 <span class="font-bold text-secondary">J U R I S F L O W</span>
             </a>
         </div>
-        <div class="mr-2 flex w-full items-center justify-end">
-            <div class="bg-slate-900 border-2 border-secondary-hover h-10 w-10 m-2 rounded-lg flex justify-center">
-                <span class="font-extrabold text-xl text-primary flex justify-center items-center w-full">{{ siglaNome }}</span>
+        <div class="mr-2 w-full flex">
+            <div class=" flex w-full justify-end">
+                <div
+                    class=" flex cursor-pointer items-center text-secondary hover:text-secondary-hover"
+                    @click="openMenuProfile = !openMenuProfile"
+                >
+                    <div
+                        class="m-2 flex h-10 w-10 justify-center rounded-lg bg-[#1F2937]"
+                        @click="openMenu = !openMenu"
+                    >
+                        <span
+                            class="flex w-full items-center justify-center text-xl font-extrabold text-[#F4D56B]"
+                            >{{ siglaNome }}</span
+                        >
+                    </div>
+                    <span class="mr-4">{{ userAbreviado }}</span>
+                </div>
             </div>
-            <span class="mr-4 text-secondary">{{ user.name }}</span>
-            <Link
-                class="pi pi-sign-out cursor-pointer rounded-lg  p-2 text-secondary hover:text-secondary-hover"
-                title='Sair'
-                :href="logout()"
-            ></Link>
+
+                <Link
+                    class="pi pi-sign-out cursor-pointer rounded-lg p-2 text-secondary hover:text-secondary-hover"
+                    title="Sair"
+                    :href="logout()"
+                ></Link>
+            </div>
         </div>
     </div>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'auth/Login')->name('home');
+Route::get('/', function(){
+    if(Auth::check()){
+        return redirect()->route('dashboard');
+    }
+    return redirect()->route('login');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
